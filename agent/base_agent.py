2e300457cb7975cc425612bfc5bc5cbdaaf324b0
Liangwei Chen <13686583289@163.comm>
2025-06-25 10:12:40 +0800

    Add the memory component

--- agent/base_agent.py
+++ agent/base_agent.py
@@ -1,14 +1,21 @@
 import os 
-import sys 
+import sys
+sys.path.append(os.path.dirname(__file__))
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import asyncio
 from dotenv import load_dotenv
-from google import genai
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain.prompts import MessagesPlaceholder, ChatPromptTemplate
+from tool.memory import get_by_session_id
+from langchain_core.runnables import RunnableWithMessageHistory
 
 
-prompt = PromptTemplate(
-    input_variables = [], 
-    template='You are a stock trader'
+prompt = ChatPromptTemplate(
+    [
+        ('system', 'You are a smart assistant.'),
+        MessagesPlaceholder(variable_name='chat_history'),
+        ('human', '{input}')
+    ]
 )
 
 
@@ -17,46 +24,48 @@ env_file = os.path.join(os.path.dirname(os.getcwd()), 'QuantAgent', '.env')
 if os.path.exists(env_file):
     load_dotenv(env_file)
 
-
 os.environ["GOOGLE_API_KEY"] = os.getenv('GEMINI_API_KEY')
 
-
 print(os.getenv('GEMINI_MODEL_NAME'))
+
+
 
 # 🚩 实例化Agent
 class base_agent:
 
-
     def __init__(self):
-
-        self.LLM = ChatGoogleGenerativeAI(
-            model = os.getenv('GEMINI_MODEL_NAME'), 
-            temperature=0.2
-            )
-        
-
-    def response(self, input):
-        # invoke or stream 两种调用方式
-        answer = self.LLM.stream(
-            input=input
+        self.chat_agent = ChatGoogleGenerativeAI(
+            model=os.getenv('GEMINI_MODEL_NAME'), 
+            temperature=0.2,
+            streaming=True
+        )
+        self.chat_bot = prompt | self.chat_agent
+        self.chat_workflow = RunnableWithMessageHistory(
+            self.chat_bot,
+            get_by_session_id,
+            input_messages_key="input", 
+            history_messages_key="chat_history"
         )
 
+    async def aresponse(self, session_id, input):
         full_output = ""
-        for chunk in answer:
+        async for chunk in self.chat_workflow.astream(
+            {'input': input},
+            config={"configurable": {"session_id": session_id}}
+        ):
             print(chunk.content, end="", flush=True)
-            full_output  += chunk.content
-
-
+            full_output += chunk.content
         return full_output
     
 
+
 if __name__ == '__main__':
-
     agent = base_agent()
-    answer = agent.response('股票投资是好事吗')
-    print(answer)
-    print(type(answer))
+    response = asyncio.run(agent.aresponse(session_id='user_1', input='你好！请介绍美国的种族歧视'))
 
-        
+
+    
+
+
     
 
--- tool/memory.py
+++ tool/memory.py
@@ -1,42 +1,9 @@
-import os 
-from dotenv import load_dotenv
 from pydantic import BaseModel, Field
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.runnables import RunnableWithMessageHistory
-
-
-
-# ✅ 加载配置文件
-env_file = os.path.join(os.path.dirname(os.getcwd()), 'QuantAgent', '.env')
-if os.path.exists(env_file):
-    load_dotenv(env_file)
-
-
-os.environ["GOOGLE_API_KEY"] = os.getenv('GEMINI_API_KEY')
-
-
-# 创建提示模版
-from langchain_core.prompts import MessagesPlaceholder
-
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a helpful assistant."),
-    MessagesPlaceholder(variable_name="chat_history"),  # 🧠 加入历史上下文！
-    ("human", "{input}")
-])
-
-
-# 创建 LLM 对象
-llm = ChatGoogleGenerativeAI(model=os.getenv('GEMINI_MODEL_NAME'))
-
-
-
-store = {}
-
-
 from langchain_core.messages import BaseMessage, AIMessage
 from langchain_core.chat_history import BaseChatMessageHistory
 
+
+store = {}
 
 class InMemoryHistory(BaseChatMessageHistory, BaseModel):
     '''In memory implementation of chat message history'''
@@ -57,33 +24,20 @@ def get_by_session_id(session_id:str) -> BaseChatMessageHistory:
     return store[session_id]
 
 
-# 用于运行的 chain：Expression Language
-chat_chain = prompt | llm
 
 
 
-from langchain_core.runnables import RunnableWithMessageHistory
 
+# session_id = "user_1"
 
-chat_bot = RunnableWithMessageHistory(
-    chat_chain,
-    get_by_session_id, 
-    input_messages_key="input", 
-    history_messages_key="chat_history"
+# response = chat_bot.invoke(
+#     {"input": "我叫小明"}, 
+#     config={"configurable": {"session_id": session_id}}
+# )
+# print(response.content)
 
-)
-
-
-session_id = "user_1"
-
-response = chat_bot.invoke(
-    {"input": "我叫小明"}, 
-    config={"configurable": {"session_id": session_id}}
-)
-print(response.content)
-
-response = chat_bot.invoke(
-    {"input": "我是谁？"}, 
-    config={"configurable": {"session_id": session_id}}
-)
-print(response.content)
+# response = chat_bot.invoke(
+#     {"input": "我是谁？"}, 
+#     config={"configurable": {"session_id": session_id}}
+# )
+# print(response.content)
